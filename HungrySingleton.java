47b43f15556075551559e82e4a348ea2d755e8e7
teletubbies-c <54931809+teletubbies-c@users.noreply.github.com>
2019-09-22 12:50:18 +0800

    Delete HungrySingleton.java

--- HungrySingleton.java
+++
@@ -1,9 +0,0 @@
-public class HungrySingleton
-{
-	private static  HungrySingleton Instance=new HungrySingleton();
-	private HungrySingleton(){};
-	public static HungrySingleton getInstance()
-	{
-		return Instance;
-	}
-}
