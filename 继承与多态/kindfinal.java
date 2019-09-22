6e1dc71fdc3cfb000cc3e45be7db754c4c10fe2c
unknown <you@example.com2210252947@qq.com>
2019-09-22 20:30:36 +0800

    类被final关键字修饰后，该类将不可以被继承

---
+++ 继承与多态/kindfinal.java
@@ -0,0 +1,19 @@
+//使用final关键字修饰Anima类
+final class Animal
+{
+	//方法体为空
+}
+//Dog类继承Animal类
+class Dog extends Animal
+{
+	//方法体为空 
+}
+//定义测试类
+class kindfinal
+{
+	public static void main(String[] args)
+	{
+		Dog dog=new Dog();//创建Dog类的实例
+	}
+	
+}
