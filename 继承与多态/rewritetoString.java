9b7a9f22c2a61df34445c63fa0537e1c20963fdf
unknown <you@example.com2210252947@qq.com>
2019-09-23 20:40:54 +0800

    重写Object的toString（）方法

---
+++ 继承与多态/rewritetoString.java
@@ -0,0 +1,18 @@
+//定义Animal类
+class Animal
+{
+	//重写Object类的toString（）方法
+	public String toString()
+	{
+		return "I am an Animal";
+	}
+}
+//定义测试类
+public class rewritetoString
+{
+	public static  void main(String[] args)
+	{
+		Animal animal=new Animal();//创建Animal对象
+		System.out.println(animal.toString());//打印animal的toString（）方法的返回值
+	}
+}
