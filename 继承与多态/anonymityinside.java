feb443c68c9615b3e9cd5745c99795d13ba98775
unknown <you@example.com2210252947@qq.com>
2019-09-23 22:08:20 +0800

    匿名内部类来实现接口

---
+++ 继承与多态/anonymityinside.java
@@ -0,0 +1,25 @@
+//定义动物类接口
+interface Animal
+{
+	void shout();//定义方法shout（）
+}
+public class anonymityinside
+{
+	public static void main(String[] args)
+	{
+	    //定义匿名内部类作为参数传递给animalShout（）方法	
+		animalShout(new Animal()
+		{
+			//实现shout（）方法
+			public void shout()
+			{
+				System.out.println("喵喵····");
+			}
+		});
+	}
+	//定义静态方法animalShout（）
+	public static void animalShout(Animal an)
+	{
+		an.shout();//调用传入对象an的shout（）方法
+	}
+}
