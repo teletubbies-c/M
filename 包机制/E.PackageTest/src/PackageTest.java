79a8f754f7fa6954c9e14338e7be066c0c045512
unknown <you@example.com2210252947@qq.com>
2019-10-05 18:36:59 +0800

    Eclipse下运行的老师教程学生

---
+++ 包机制/E.PackageTest/src/PackageTest.java
@@ -0,0 +1,14 @@
+import course.*;
+import teacher.*;
+import student.*;
+
+public class PackageTest {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Student st=new Student();
+		Teacher te=new Teacher();
+		Course co=new Course();
+	}
+
+}
