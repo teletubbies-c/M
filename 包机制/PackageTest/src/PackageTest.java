73851849f59bfd302e356b8604767d1896ae5dca
unknown <you@example.com2210252947@qq.com>
2019-10-05 17:25:34 +0800

    命令行模式运行老师学生课程

---
+++ 包机制/PackageTest/src/PackageTest.java
@@ -0,0 +1,13 @@
+import course.*;
+import teacher.*;
+import student.*;
+
+public class PackageTest
+{
+	public static void main(String[] args)
+	{
+		Student st=new Student();
+		Teacher te=new Teacher();
+		Course co=new Course();
+	}
+}
