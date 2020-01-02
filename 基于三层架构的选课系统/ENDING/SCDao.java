f9efbdebcd889b96fd63431ebd8f7eb71017ac72
teletubbies-c <54931809+teletubbies-c@users.noreply.github.com>
2020-01-02 21:08:34 +0800

    采购员

---
+++ 基于三层架构的选课系统/ENDING/SCDao.java
@@ -0,0 +1,69 @@
+package dao;
+import entity.IEntity;
+import entity.SC;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class SCDao implements IDao,Serializable {
+	private static SCDao instance;
+	private HashMap<String, IEntity> SCs;
+	private SC SC;
+	private SCDao() {
+		SCs = new HashMap<String, IEntity>();
+		SC sc= new SC();
+		
+		sc.setStudentNo("164801001");
+		sc.addone("0001",2);
+		sc.addone("0002",2);
+		SCs.put(sc.getStudentNo(), sc);
+	}
+	public static SCDao getInstance() {
+		if(instance == null) {
+			synchronized(SCDao.class) {
+				if(instance == null) {
+					instance = new SCDao();
+					return instance;
+				}
+			}
+		}
+		return instance;
+	}
+	@Override
+	public void insert(IEntity entity) {
+		// TODO Auto-generated method stub
+		SC st = (SC)entity;
+		SCs.put(st.getStudentNo(), st);
+	}
+
+	@Override
+	public void delete(String id){
+		SCs.remove(id);// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(IEntity entity) {
+		SC st = (SC)entity;
+		SCs.put(st.getStudentNo(), st);// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public HashMap<String, IEntity> getAllEntities() {
+		// TODO Auto-generated method stub
+		return SCs;
+		//返回集合中所有的学生信息
+	}
+
+	@Override
+	public IEntity getEntity(String Id) {
+		// TODO Auto-generated method stub		
+		return SCs.get(Id);
+	}
+
+
+	
+	
+
+}
